5556b29001e6d22152ed7501489f4241cd5984ad
Felipe Maia <felipe.cmaia@hotmail.com>
2022-12-07 21:18:32 -0300

    Encapsulation of properties

--- PaymentContext/PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Student.cs
@@ -2,10 +2,32 @@ namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public List<Subscription> Subscriptions { get; set; }
+        private IList<Subscription> _subscriptions;
+
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            // Desativar assinaturas ativas
+            foreach (var subs in _subscriptions)
+            {
+                subs.Inactivate();
+            }
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
---
+++ PaymentContext/PaymentContext.Tests/Entites/StudentTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Felipe", "Maia", "221232", "teste@teste.com");
+        student.AddSubscription(subscription);
+    }
+}
